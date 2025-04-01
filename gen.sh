fad12f3a77d0ee4c6924860e0a81d15285a3318b
Michael Woolnough <michael.woolnough@gmail.com>
2025-04-01 20:13:06 +0100

    cleanup of generator code

--- gen.sh
+++ gen.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
+numberTypes() {
+	for t in "Int" "Uint" "Float"; do
+		declare types="8 16 24 32 40 48 56 64";
+
+		if [ "$t" = "Float" ]; then
+			types="32 64";
+		fi;
+
+		for i in $types; do
+			declare ti=$i;
+
+			if [ $i -eq 24 ]; then
+				ti=32;
+			elif [ $i -gt 32 ]; then
+				ti=64;
+			fi;
+
+			echo "$t $i $ti";
+		done;
+	done;
+}
+
+readWrite() {
+	echo "Read er";
+	echo "Write r";
+}
+
 for s in "" Sticky; do
 	for e in Big Little; do
 		declare startP;
 		declare order;
+
 		if [ "$e" = "Big" ]; then
 			startP=1;
 			order=-1;
@@ -12,11 +40,7 @@ for s in "" Sticky; do
 			order=1;
 		fi;
 
-		for rw in Read Write; do
-			er="er";
-			if [ "$rw" = "Write" ]; then
-				er="r";
-			fi;
+		readWrite | while read rw er; do
 			(
 				cat <<HEREDOC
 package byteio
@@ -57,6 +81,7 @@ HEREDOC
 					else
 						echo "	n, e.Err = e.Writer.Write(p)";
 					fi;
+
 					cat <<HEREDOC
 	e.Count += int64(n)
 
@@ -72,12 +97,17 @@ HEREDOC
 				echo -n "func (e *${s}${e}Endian${rw}${er}) ${rw}Bool(";
 				if [ "${rw}" = "Write" ]; then
 					echo -n "b bool";
+
 					ret="";
+
 					if [ -z "$s" ]; then
-						echo -n ") (int, error"
+						echo -n ") (int, error";
+
 						ret="return ";
 					fi;
+
 					echo ") {"
+
 					cat <<HEREDOC
 	if b {
 		${ret}e.WriteUint8(1)
@@ -87,193 +117,211 @@ HEREDOC
 }
 HEREDOC
 				else
-					echo -n ") "
+					echo -n ") ";
+
 					if [ -z "$s" ]; then
-						echo "(bool, int, error) {"
+						echo "(bool, int, error) {";
 						echo "	b, n, err := e.ReadUint8()";
 						echo;
 						echo "	return b != 0, n, err";
 						echo "}";
 					else
-						echo "bool {"
+						echo "bool {"a;
 						echo "	return e.ReadUint8() != 0";
 						echo "}";
 					fi;
 				fi;
 
-				for t in "Int" "Uint" "Float"; do
-					types="8 16 24 32 40 48 56 64"
-					if [ "$t" = "Float" ]; then
-						types="32 64";
+				numberTypes | while read t i ti; do
+					declare tu="${t@u}$ti";
+
+					echo;
+					echo "// ${rw}${t}${i} ${rw}s a $i bit ${t@L} as a ${tu@L} using the underlying io.${rw}${er}.";
+
+					if [ ! -z "$s" ]; then
+						echo "// Any errors and the running byte read count are stored instead of being returned.";
 					fi;
-					for i in $types; do
-						ti=$i;
-						if [ $i -eq 24 ]; then
-							ti=32;
-						elif [ $i -gt 32 -a $i -lt 64 ]; then
-							ti=64;
-						fi;
-						tu="$(echo "$t" | tr A-Z a-z)$ti";
-						echo;
-						echo "// ${rw}${t}${i} ${rw}s a $i bit $(echo "$t" | tr A-Z a-z) as a $tu using the underlying io.${rw}${er}.";
-						if [ ! -z "$s" ]; then
-							echo "// Any errors and the running byte read count are stored instead of being returned.";
-						fi;
-						echo -n "func (e *${s}${e}Endian${rw}${er}) ${rw}${t}${i}(";
-						if [ "$rw" = "Write" ]; then
-							echo -n "d $tu) ";
-							if [ -z "$s" ]; then
-								echo -n "(";
-							fi;
-						else
-							echo -n ") ";
-							if [ -z "$s" ]; then
-								echo -n "($tu, ";
-							else
-								echo -n "$tu ";
-							fi;
-						fi;
+
+					echo -n "func (e *${s}${e}Endian${rw}${er}) ${rw}${t}${i}(";
+
+					if [ "$rw" = "Write" ]; then
+						echo -n "d ${tu@L}) ";
+
 						if [ -z "$s" ]; then
-							echo "int, error) {";
-						else
-							echo "{";
+							echo -n "(";
 						fi;
+					else
+						echo -n ") ";
+						if [ -z "$s" ]; then
+							echo -n "(${tu@L}, ";
+						else
+							echo -n "${tu@L} ";
+						fi;
+					fi;
+
+					if [ -z "$s" ]; then
+						echo "int, error) {";
+					else
+						echo "{";
+					fi;
+
+					if [ ! -z "$s" ]; then
+						echo "	if e.Err != nil {";
+						echo -n "		return";
+
+						if [ "$rw" = "Read" ]; then
+							echo -n " 0";
+						fi;
+
+						echo;
+						echo "	}";
+						echo;
+					fi;
+
+					if [ "$rw" = "Read" ]; then
+						echo "	n, err := io.ReadFull(e.Reader, e.buffer[:$(( $i / 8 ))])";
 
 						if [ ! -z "$s" ]; then
-							echo "	if e.Err != nil {";
-							echo -n "		return";
-							if [ "$rw" = "Read" ]; then
-								echo -n " 0";
-							fi;
 							echo;
-							echo "	}";
+							echo "	e.Count += int64(n)";
 							echo;
 						fi;
-						if [ "$rw" = "Read" ]; then
-							echo "	n, err := io.ReadFull(e.Reader, e.buffer[:$(( $i / 8 ))])";
-							if [ ! -z "$s" ]; then
-								echo;
-								echo "	e.Count += int64(n)";
-								echo;
-							fi;
-							echo "	if err != nil {";
-							if [ -z "$s" ]; then
-								echo "		return 0, n, err";
-							else
-								echo "		e.Err = err";
-								echo;
-								echo "		return 0";
-							fi;
-					
-							echo "	}";
-							echo ;
-							echo -n "	return ";
 
-							if [ "$t" = "Int" ]; then
-								echo -n "int$ti(";
-							elif [ "$t" = "Float" ]; then
-								echo -n "math.Float${i}frombits(";
+						echo "	if err != nil {";
+
+						if [ -z "$s" ]; then
+							echo "		return 0, n, err";
+						else
+							echo "		e.Err = err";
+							echo;
+							echo "		return 0";
+						fi;
+
+						echo "	}";
+						echo ;
+						echo -n "	return ";
+
+						if [ "$t" = "Int" ]; then
+							echo -n "int$ti(";
+						elif [ "$t" = "Float" ]; then
+							echo -n "math.Float${i}frombits(";
+						fi;
+
+						p=$(( $startP * $i / 8 - startP));
+
+						shift=0;
+
+						for n in $(seq 1 $(( $i / 8 ))); do
+							if [ $n -ne 1 ]; then
+								echo -n " | ";
 							fi;
-							
-							p=$(( $startP * $i / 8 - startP));
-							shift=0;
 
-							for n in $(seq 1 $(( $i / 8 ))); do
-								if [ $n -ne 1 ]; then
-									echo -n " | ";
-								fi;
-								if [ "$i" -ne 8 ]; then
-									echo -n "uint$ti(";
-								fi;
-								echo -n "e.buffer[$p]";
-								if [ "$i" -ne 8 ]; then
-									echo -n ")";
-								fi;
-								if [ $shift -gt 0 ]; then
-									echo -n "<<$shift";
-								fi;
-								let "shift += 8";
-								let "p += order";
-							done;
+							if [ "$i" -ne 8 ]; then
+								echo -n "uint$ti(";
+							fi;
 
-							if [ "$t" != "Uint" ]; then
+							echo -n "e.buffer[$p]";
+
+							if [ "$i" -ne 8 ]; then
 								echo -n ")";
 							fi;
 
-							if [ -z "$s" ]; then
-								echo ", $(( $i / 8 )), nil";
-							else
-								echo;
-							fi;
-						else
-							var="d";
-							if [ $i -eq 8 ]; then
-								if [ "$t" = "Int" ]; then
-									echo "	e.buffer[0] = byte(d)";
-								else
-									echo "	e.buffer[0] = d";
-								fi;
-							else
-								if [ "$t" = "Float" ]; then
-									var="c";
-									echo "	c := math.Float${i}bits(d)";
-								elif [ "$t" = "Int" ]; then
-									var="c";
-									echo "	c := uint${ti}(d)";
-								fi;
-								echo "	e.buffer = [9]byte{";
-
-								shift=0;
-
-								if [ $order -eq -1 ]; then
-									shift=$(( ((i / 8) - 1) * 8));
-								fi;
-
-								for n in $(seq $(( i / 8 ))); do
-									echo -n "		byte(";
-									echo -n "$var";
-									if [ $shift -ne 0 ]; then
-										echo -n " >> $shift";
-									fi;
-									echo "),";
-									let "shift += 8 * $order";
-								done;
-
-								echo "	}";
+							if [ $shift -gt 0 ]; then
+								echo -n "<<$shift";
 							fi;
 
-							if [ -z "$s" ]; then
-								echo;
-								echo -n "	return ";
-							else
-								echo;
-								echo "	var n int";
-								echo;
-								echo -n "	n, e.Err = ";
-							fi;
-							
-							echo  "e.Writer.Write(e.buffer[:$(( $i / 8 ))])";
-							
-							if [ ! -z "$s" ]; then
-								echo "	e.Count += int64(n)";
-							fi;
+							let "shift += 8";
+							let "p += order";
+						done;
+
+						if [ "$t" != "Uint" ]; then
+							echo -n ")";
 						fi;
 
-						echo "}";
-					done;
+						if [ -z "$s" ]; then
+							echo ", $(( $i / 8 )), nil";
+						else
+							echo;
+						fi;
+					else
+						declare var="d";
+
+						if [ $i -eq 8 ]; then
+							if [ "$t" = "Int" ]; then
+								echo "	e.buffer[0] = byte(d)";
+							else
+								echo "	e.buffer[0] = d";
+							fi;
+						else
+							if [ "$t" = "Float" ]; then
+								var="c";
+								echo "	c := math.Float${i}bits(d)";
+							elif [ "$t" = "Int" ]; then
+								var="c";
+								echo "	c := uint${ti}(d)";
+							fi;
+
+							echo "	e.buffer = [9]byte{";
+
+							shift=0;
+
+							if [ $order -eq -1 ]; then
+								shift=$(( ((i / 8) - 1) * 8));
+							fi;
+
+							for n in $(seq $(( i / 8 ))); do
+								echo -n "		byte(";
+								echo -n "$var";
+
+								if [ $shift -ne 0 ]; then
+									echo -n " >> $shift";
+								fi;
+
+								echo "),";
+
+								let "shift += 8 * $order";
+							done;
+
+							echo "	}";
+						fi;
+
+						if [ -z "$s" ]; then
+							echo;
+							echo -n "	return ";
+						else
+							echo;
+							echo "	var n int";
+							echo;
+							echo -n "	n, e.Err = ";
+						fi;
+
+						echo  "e.Writer.Write(e.buffer[:$(( $i / 8 ))])";
+
+						if [ ! -z "$s" ]; then
+							echo "	e.Count += int64(n)";
+						fi;
+					fi;
+
+					echo "}";
 				done;
-				type="[]byte";
+
+				declare type="[]byte";
+
 				for t in Bytes String; do
-					tt="$t";
+					declare tt="$t";
+
 					if [ "$tt" = "Bytes" ]; then
 						tt="";
 					fi;
+
 					if [ "$t" = "String" ] || [ "$rw" = "Read" ]; then
 						echo;
 						echo "// ${rw}${t} ${rw}s a ${type}.";
 						echo -n "func (e *${s}${e}Endian${rw}${er}) ${rw}${t}(";
+
 						if [ "$rw" = "Write" ]; then
 							echo "d ${type}) (int, error) {";
+
 							if [ -z "$s" ]; then
 								if [ "$t" = "String" ]; then
 									echo "	return io.WriteString(e.Writer, d)";
@@ -286,22 +334,26 @@ HEREDOC
 								echo "	}";
 								echo;
 								echo "	var n int";
+
 								if [ "$t" = "String" ]; then
 									echo "	n, e.Err = io.WriteString(e.Writer, d)";
 								else
 									echo "	n, e.Err = e.Write(d)";
 								fi;
+
 								echo "	e.Count += int64(n)";
 								echo;
 								echo "	return n, e.Err";
 							fi;
 						else
 							echo -n "size int) ";
+
 							if [ -z "$s" ]; then
 								echo "(${type}, int, error) {";
 								echo "	buf := make([]byte, size)";
 								echo "	n, err := io.ReadFull(e, buf)";
 								echo;
+
 								if [ "$t" = "String" ]; then
 									echo "	return string(buf[:n]), n, err";
 								else
@@ -310,11 +362,13 @@ HEREDOC
 							else
 								echo "${type} {";
 								echo "	if e.Err != nil {";
+
 								if [ "$t" = "String" ]; then
 									echo "		return \"\"";
 								else
 									echo "		return nil";
 								fi;
+
 								echo "	}";
 								echo;
 								echo "	buf := make([]byte, size)";
@@ -323,6 +377,7 @@ HEREDOC
 								echo "	n, e.Err = io.ReadFull(e.Reader, buf)";
 								echo "	e.Count += int64(n)";
 								echo;
+
 								if [ "$t" = "String" ]; then
 									echo "	return string(buf[:n])";
 								else
@@ -330,20 +385,25 @@ HEREDOC
 								fi;
 							fi;
 						fi;
+
 						echo "}";
 					fi;
+
 					for size in "X" 8 16 24 32 40 48 56 64; do
 						tSize="$size";
+
 						if [ "$size" = "X" ]; then
 							tSize="64";
 						elif [ $size -eq 24 ]; then
 							tSize=32;
-						elif [ $size -gt 32 -a $size -lt 64 ]; then
+						elif [ $size -gt 32 ]; then
 							tSize=64;
 						fi;
+
 						echo;
 						echo "// ${rw}${t}${size} ${rw}s the length of the ${t}, using ReadUint${size} and then ${rw}s the bytes.";
 						echo -n "func (e *${s}${e}Endian${rw}${er}) ${rw}${t}${size}(";
+
 						if [ "$rw" = "Write" ]; then
 							if [ -z "$s" ]; then
 								echo "p ${type}) (int, error) {";
@@ -358,21 +418,23 @@ HEREDOC
 							else
 								echo "p ${type}) {";
 								echo "	e.WriteUint${size}(uint${tSize}(len(p)))";
-								echo "	e.Write${tt}(p)"
+								echo "	e.Write${tt}(p)";
 							fi;
 						else
 							if [ -z "$s" ]; then
 								echo ") (${type}, int, error) {";
 								echo "	size, n, err := e.ReadUint${size}()";
-								echo "	if err != nil {"
+								echo "	if err != nil {";
+
 								if [ "$t" = "String" ]; then
 									echo "		return \"\", n, err";
 								else
 									echo "		return nil, n, err";
 								fi;
+
 								echo "	}";
 								echo;
-								echo "	p, m, err := e.Read${t}(int(size))"
+								echo "	p, m, err := e.Read${t}(int(size))";
 								echo;
 								echo "	return p, n + m, err";
 							else
@@ -380,18 +442,24 @@ HEREDOC
 								echo "	return e.Read${t}(int(e.ReadUint${size}()))";
 							fi;
 						fi;
+
 						echo "}";
 					done;
+
 					type="string";
 				done;
+
 				echo;
 				echo -n "// ${rw}String0 ${rw}s the bytes of the string ";
+
 				if [ "$rw" = "Write" ]; then
 					echo "ending with a 0 byte.";
 				else
 					echo "until a 0 byte is read.";
 				fi;
+
 				echo -n "func (e *${s}${e}Endian${rw}${er}) ${rw}String0(";
+
 				if [ "$rw" = "Write" ]; then
 					if [ -z "$s" ]; then
 						echo "p string) (int, error) {";
@@ -450,6 +518,7 @@ HEREDOC
 						echo "	return string(d)";
 					fi;
 				fi;
+
 				echo "}";
 			) > "$(echo "${s}${e}Endian${rw}${er}" | tr A-Z a-z).go";
 		done;
