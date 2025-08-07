db3ce685ceaf78f60a15fe4cc6479076f72e06d9
Michael Woolnough <michael.woolnough@gmail.com>
2025-08-07 07:15:44 +0100

    added more var uint tests

--- varint_test.go
+++ varint_test.go
@@ -16,6 +16,7 @@ func TestLittleEndianVarUint(t *testing.T) {
 		{127, []byte{127}},
 		{128, []byte{128, 0}},
 		{129, []byte{129, 0}},
+		{163, []byte{163, 0}},
 		{255, []byte{255, 0}},
 		{256, []byte{128, 1}},
 		{257, []byte{129, 1}},
@@ -43,11 +44,61 @@ func TestLittleEndianVarUint(t *testing.T) {
 		buf.Reset()
 		(&LittleEndianWriter{Writer: buf}).WriteUintX(test.Input)
 		if !bytes.Equal(buf.Bytes(), test.Output) {
-			t.Errorf("test %d.1: expecting %v, got %v", n+1, test.Output, buf)
+			t.Errorf("test %d.1: expecting %v, got %v", n+1, test.Output, buf.Bytes())
 			buf.Reset()
 			buf.Write(test.Output)
 		}
 		num, _, _ := (&LittleEndianReader{Reader: buf}).ReadUintX()
+		if num != test.Input {
+			t.Errorf("test %d.2: expecting %d, got %d", n+1, test.Input, num)
+		}
+	}
+}
+
+func TestStickyLittleEndianVarUint(t *testing.T) {
+	buf := bytes.NewBuffer(make([]byte, 0, 9))
+	for n, test := range [...]struct {
+		Input  uint64
+		Output []byte
+	}{
+		{0, []byte{0}},
+		{1, []byte{1}},
+		{127, []byte{127}},
+		{128, []byte{128, 0}},
+		{129, []byte{129, 0}},
+		{163, []byte{163, 0}},
+		{255, []byte{255, 0}},
+		{256, []byte{128, 1}},
+		{257, []byte{129, 1}},
+		{300, []byte{172, 1}},
+		{512, []byte{128, 3}},
+		{1024, []byte{128, 7}},
+		{2048, []byte{128, 15}},
+		{4096, []byte{128, 31}},
+		{8192, []byte{128, 63}},
+		{16384, []byte{128, 127}},
+		{16511, []byte{255, 127}},
+		{16512, []byte{128, 128, 0}},
+		{32768, []byte{128, 255, 0}},
+		{65536, []byte{128, 255, 2}},
+		{131072, []byte{128, 255, 6}},
+		{262144, []byte{128, 255, 14}},
+		{524288, []byte{128, 255, 30}},
+		{1048576, []byte{128, 255, 62}},
+		{2097152, []byte{128, 255, 126}},
+		{134217728, []byte{128, 255, 254, 62}},
+		{268435456, []byte{128, 255, 254, 126}},
+		{17179869184, []byte{128, 255, 254, 254, 62}},
+		{18446744073709551615, []byte{255, 254, 254, 254, 254, 254, 254, 254, 254}},
+	} {
+		buf.Reset()
+		(&StickyLittleEndianWriter{Writer: buf}).WriteUintX(test.Input)
+		if !bytes.Equal(buf.Bytes(), test.Output) {
+			t.Errorf("test %d.1: expecting %v, got %v", n+1, test.Output, buf)
+			buf.Reset()
+			buf.Write(test.Output)
+		}
+		num := (&StickyLittleEndianReader{Reader: buf}).ReadUintX()
 		if num != test.Input {
 			t.Errorf("test %d.2: expecting %d, got %d", n+1, test.Input, num)
 		}
